b08648e7f25b3e24d71d0c9c42f873b72dc64c55
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2023-10-07 15:18:59 +0100

    Implemented collision checking

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::collide_aabb::{collide, Collision}};
 
 #[derive(Bundle)]
 struct PlayerBundle {
@@ -25,7 +25,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+           .add_systems(Update, collision_check);
     }
 }
 
@@ -40,6 +41,7 @@ fn setup(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn(PlayerBundle {
         marker: Player,
         sprite: SpriteBundle { 
+            sprite: Sprite { custom_size: Some(Vec2::new(48.0, 48.0)), ..default() },
             transform: Transform { translation: Vec3::new(0.0, 0.0, 0.0), 
                                 scale: Vec3::new(1.0, 1.0, 1.0),
                                 ..default() },
@@ -49,6 +51,7 @@ fn setup(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn(GroundBundle {
         marker: Ground,
         sprite: SpriteBundle { 
+            sprite: Sprite { custom_size: Some(Vec2::new(48.0, 48.0)), ..default() },
             transform: Transform { translation: Vec3::new(0.0, -144.0, 0.0), 
                                 scale: Vec3::new(1.0, 1.0, 1.0),
                                 ..default() },
@@ -56,4 +59,19 @@ fn setup(mut commands: Commands, server: Res<AssetServer>) {
         }
     });
     commands.spawn((Camera2dBundle::default(), Camera));
+}
+
+fn collision_check(ground: Query<(&Sprite, &Transform), With<Ground>>, mut player: Query<(&Sprite, &mut Transform), (With<Player>, Without<Ground>)>) {
+    for (ground_sprite, ground_transform) in ground.iter() {
+        for (player_sprite, mut player_transform) in player.iter_mut() {
+            let player_pos = player_transform.translation;
+            let player_size = player_sprite.custom_size.unwrap_or_default();
+            let ground_pos = ground_transform.translation;
+            let ground_size = ground_sprite.custom_size.unwrap_or_default();
+            let collision = collide(player_pos, player_size, ground_pos, ground_size);
+            if collision.is_some_and(|c| c == Collision::Top) {
+                player_transform.translation = Vec3::new(player_pos.x, ground_pos.y + ((ground_size.y + player_size.y) / 2.0), player_pos.z);
+            }
+        }
+    }
 }
