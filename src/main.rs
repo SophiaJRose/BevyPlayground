c2d7f0ae461a24b65f38fa21ec246d7415e1c488
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2023-10-08 10:34:53 +0100

    Refactored input handling

--- src/main.rs
+++ src/main.rs
@@ -57,23 +57,40 @@ struct SavePointBundle {
 #[derive(Component)]
 struct SavePoint;
 
+#[derive(Default)]
+struct ButtonState {
+    pressed: bool,
+    just_pressed: bool,
+    just_released: bool
+}
+
+#[derive(Resource, Default)]
+struct InputState {
+    left: ButtonState,
+    right: ButtonState,
+    jump: ButtonState,
+    save: ButtonState,
+    load: ButtonState
+}
+
 const GRAVITY: f32 = 1.0;
 const MOVE_SPEED: f32 = 6.0;
 const RUN_SPEED: f32 = 9.0;
 const JUMP_SPEED: f32 = 20.0;
 
-pub struct HelloPlugin;
+pub struct GamePlugin;
 
-impl Plugin for HelloPlugin {
+impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
-           .add_systems(Update, (movement, collision_check, savepoint));
+        app.init_resource::<InputState>()
+           .add_systems(Startup, setup)
+           .add_systems(Update, (input_handling, movement, collision_check, savepoint));
     }
 }
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins((DefaultPlugins, GamePlugin))
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
@@ -222,43 +239,39 @@ fn collision_check(ground: Query<(&Sprite, &Transform, &DeathPlane), With<Ground
     }
 }
 
-fn movement(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), With<Player>>, keys: Res<Input<KeyCode>>) {
+fn movement(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), With<Player>>, input: Res<InputState>) {
     for (mut transform, mut velocity, mut state, mut run_timer, mut wall_jump_timer) in player.iter_mut() {
         let player_pos = transform.translation;
         let mut new_x_velocity = velocity.0.x;
         let mut new_y_velocity = velocity.0.y - if matches!(state.0, StateEnum::WallSliding) {GRAVITY / 2.0} else {GRAVITY};
-        let left_pressed = keys.pressed(KeyCode::A) || keys.pressed(KeyCode::Left);
-        let right_pressed = keys.pressed(KeyCode::D) || keys.pressed(KeyCode::Right);
         // Move left or right
-        if left_pressed {
+        if input.left.pressed {
             if run_timer.0 <= 30.0 && !matches!(state.0, StateEnum::WallJumping) {
                 new_x_velocity = -MOVE_SPEED;
             } else {
                 new_x_velocity = -RUN_SPEED;
             }
         }
-        if right_pressed  {
+        if input.right.pressed {
             if run_timer.0 <= 30.0 && !matches!(state.0, StateEnum::WallJumping) {
                 new_x_velocity = MOVE_SPEED;
             } else {
                 new_x_velocity = RUN_SPEED;
             }
         }
-        if left_pressed == right_pressed {
+        if input.left.pressed == input.right.pressed {
             new_x_velocity = 0.0;
             run_timer.0 = 0.0;
         } else if matches!(state.0, StateEnum::Grounded) {
             run_timer.0 += 1.0;
         }
-        let jump_just_pressed = keys.just_pressed(KeyCode::W) || keys.just_pressed(KeyCode::Space) || keys.just_pressed(KeyCode::Up);
-        let jump_pressed = keys.pressed(KeyCode::W) || keys.pressed(KeyCode::Space) || keys.pressed(KeyCode::Up);
         // Jump
-        if jump_just_pressed && matches!(state.0, StateEnum::Grounded) {
+        if input.jump.just_pressed && matches!(state.0, StateEnum::Grounded) {
             new_y_velocity = JUMP_SPEED;
             state.0 = StateEnum::Jumping;
         }
         // Wall Jump
-        if jump_just_pressed && matches!(state.0, StateEnum::WallSliding) {
+        if input.jump.just_pressed && matches!(state.0, StateEnum::WallSliding) {
             // Will be either 1 or -1 depending on whether velocity is positive or negative 
             velocity.0.x = RUN_SPEED * velocity.0.x.signum() * -1.0;
             new_y_velocity = JUMP_SPEED;
@@ -266,7 +279,7 @@ fn movement(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, 
             wall_jump_timer.0 = 15.0;
         }
         // If jump key released before apex, half upward velocity to shorten jump
-        if !jump_pressed && new_y_velocity >= 0.0 {
+        if !input.jump.pressed && new_y_velocity >= 0.0 {
             new_y_velocity /= 2.0;
         }
         if wall_jump_timer.0 > 0.0 {
@@ -280,17 +293,17 @@ fn movement(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, 
     }
 }
 
-fn savepoint(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), (With<Player>, Without<SavePoint>)>, mut save_point: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), (With<SavePoint>, Without<Player>)>, keys: Res<Input<KeyCode>>) {
+fn savepoint(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), (With<Player>, Without<SavePoint>)>, mut save_point: Query<(&mut Transform, &mut Velocity, &mut PlayerState, &mut RunTimer, &mut WallJumpTimer), (With<SavePoint>, Without<Player>)>, input: Res<InputState>) {
     for (mut player_transform, mut player_velocity, mut player_player_state, mut player_run_timer, mut player_wall_jump_timer) in player.iter_mut() {
         let (mut save_transform, mut save_velocity, mut save_player_state, mut save_run_timer, mut save_wall_jump_timer) = save_point.single_mut();
-        if keys.just_pressed(KeyCode::Q) {
+        if input.save.just_pressed {
             save_transform.translation = player_transform.translation;
             save_velocity.0 = player_velocity.0;
             save_player_state.0 = player_player_state.0;
             save_run_timer.0 = player_run_timer.0;
             save_wall_jump_timer.0 = player_wall_jump_timer.0;
         }
-        if keys.just_pressed(KeyCode::E) {
+        if input.load.just_pressed {
             player_transform.translation = save_transform.translation;
             player_velocity.0 = save_velocity.0;
             player_player_state.0 = save_player_state.0;
@@ -298,4 +311,27 @@ fn savepoint(mut player: Query<(&mut Transform, &mut Velocity, &mut PlayerState,
             player_wall_jump_timer.0 = save_wall_jump_timer.0;
         }
     }
+}
+
+fn input_handling(mut input: ResMut<InputState>, keys: Res<Input<KeyCode>>) {
+    // Left
+    input.left.pressed = keys.pressed(KeyCode::A) || keys.pressed(KeyCode::Left);
+    input.left.just_pressed = keys.just_pressed(KeyCode::A) || keys.just_pressed(KeyCode::Left);
+    input.left.just_released = keys.just_released(KeyCode::A) || keys.just_released(KeyCode::Left);
+    // Right
+    input.right.pressed = keys.pressed(KeyCode::D) || keys.pressed(KeyCode::Right);
+    input.right.just_pressed = keys.just_pressed(KeyCode::D) || keys.just_pressed(KeyCode::Right);
+    input.right.just_released = keys.just_released(KeyCode::D) || keys.just_released(KeyCode::Right);
+    // Jump
+    input.jump.pressed = keys.pressed(KeyCode::W) || keys.pressed(KeyCode::Up) || keys.pressed(KeyCode::Space);
+    input.jump.just_pressed = keys.just_pressed(KeyCode::W) || keys.just_pressed(KeyCode::Up) || keys.just_pressed(KeyCode::Space);
+    input.jump.just_released = keys.just_released(KeyCode::W) || keys.just_released(KeyCode::Up) || keys.just_released(KeyCode::Space);
+    // Save
+    input.save.pressed = keys.pressed(KeyCode::Q);
+    input.save.just_pressed = keys.just_pressed(KeyCode::Q);
+    input.save.just_released = keys.just_released(KeyCode::Q);
+    // Load
+    input.load.pressed = keys.pressed(KeyCode::E);
+    input.load.just_pressed = keys.just_pressed(KeyCode::E);
+    input.load.just_released = keys.just_released(KeyCode::E);
 }
